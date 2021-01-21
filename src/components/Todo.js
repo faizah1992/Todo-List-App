957236d33f8f6b56d718e283a37d0547eaceed18
Faizah Ahsan <faizah1992@gmail.com>
2021-01-21 11:54:25 -0600

    completehandler added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,6 +5,18 @@ const Todo = ({ text,todo,todos,setTodos  }) => {
     const deleteHandler = () => {
         setTodos(todos.filter(el => el.id !== todo.id))
     }
+
+    const completeHandler = () => {
+        setTodos(todos.map((item) => {
+            if(item.id === todo.id){
+                return {
+                    ...item,
+                    completed: !item.completed
+                }
+            }
+            return item
+        }))
+    }
     return(
         <div className="todo">
              {text}
